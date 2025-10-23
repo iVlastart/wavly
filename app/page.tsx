90d92a4f31ec8ef653b5377b0aaa9ad935bef5b9
Vlasta Vašut <vasutvlastimil@gmail.com>
2025-10-23 23:16:02 +0200

    Added a sidebar component. Need to adjust it to my project

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,14 @@
 'use client'
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "./firebase/firebase";
+import { Navbar } from "./ui/sidebar";
 
 export default function Home() {
   const [user] = useAuthState(auth);
   
   return (
       <>
-        <header>
-          
-        </header>
+        <Navbar/>
       </>  
     );
 }
---
+++ app/ui/sidebar.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+
+import { Sidebar, Menu, MenuItem, Submenu, Logo } from "react-mui-sidebar";
+import AccessAlarms from "@mui/icons-material/AccessAlarms";
+import CottageOutlinedIcon from "@mui/icons-material/CottageOutlined";
+import Link from "next/link";
+
+export const Navbar = () => {
+  return (
+    <Sidebar width={"270px"}>
+      <Logo
+        component={Link}
+        href="/"
+        img="https://adminmart.com/wp-content/uploads/2024/03/logo-admin-mart-news.png"
+      >
+        Wavly
+      </Logo>
+      <Menu subHeading="HOME">
+        <MenuItem
+          icon={<CottageOutlinedIcon />}
+          component={Link}
+          link="/tes"
+          badge={true}
+          isSelected={true}
+        >
+          {" "}
+          {/* Set badge to boolean true */}
+          Home
+        </MenuItem>
+        <MenuItem icon={<AccessAlarms />} component={Link} link="/test">
+          eCommerce
+        </MenuItem>
+        <MenuItem component={Link} link="/ana">
+          Analytical
+        </MenuItem>
+      </Menu>
+      <Menu subHeading="APPS">
+        <MenuItem>Chat</MenuItem>
+        <MenuItem>Calendar</MenuItem>
+      </Menu>
+      <Menu subHeading="OTHERS">
+        <Submenu title="Menu Level">
+          <MenuItem>Post</MenuItem>
+          <MenuItem>Details</MenuItem>
+          <Submenu title="Level 2">
+            <MenuItem>new</MenuItem>
+            <MenuItem>Hello</MenuItem>
+          </Submenu>
+        </Submenu>
+
+        <MenuItem>Chip</MenuItem>
+        <MenuItem target="_blank" component={Link} link="https://google.com">
+          External Link
+        </MenuItem>
+      </Menu>
+    </Sidebar>
+  );
+};
