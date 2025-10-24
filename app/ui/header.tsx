dd0ade6e53dd539e4a04d4700bb825d7a9cfcb24
Vlasta Vašut <vasutvlastimil@gmail.com>
2025-10-24 23:05:58 +0200

    Added a searchbar

--- app/ui/header.tsx
+++ app/ui/header.tsx
@@ -1,8 +1,14 @@
+import { Auth } from "./auth";
+import { Searchbar } from "./searchbar";
+
 export const Header = ()=>{
     
     return(
         <header className="w-full flex flex-row items-center border border-black pt-2">
-            
+            <div className="mx-auto">
+                <Searchbar/>
+            </div>
+            <Auth/>
         </header>
     );
 };
--- app/ui/searchbar.tsx
+++ app/ui/searchbar.tsx
@@ -1,7 +1,15 @@
 export const Searchbar = ()=>{
     return(
-        <>
-        
-        </>
+        <div className="mx-auto relative">
+            <input id="search-bar" type="search" maxLength={30} placeholder="Search" 
+                    className="px-4 py-2 w-[20rem] rounded-full focus:outline-none focus:ring-2 focus:ring-blue-500 transition-colors duration-500" 
+                    autoComplete="off" aria-label="Search"/>
+            <button id="search-btn" className="absolute right-0 top-0 h-full px-4 
+                            bg-blue-500 text-white rounded-r-full flex items-center justify-center">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
+                    <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
+                </svg>
+            </button>
+        </div>
     );
 };
