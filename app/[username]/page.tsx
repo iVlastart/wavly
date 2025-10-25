7a4e52226514793b63c771533ac340dc6e6597e6
Vlasta Vašut <vasutvlastimil@gmail.com>
2025-10-25 15:33:36 +0200

    Added navbar and header to profile. I also have remade the gettint the search param to fit the client

--- app/[username]/page.tsx
+++ app/[username]/page.tsx
@@ -1,9 +1,30 @@
+'use client'
+import { useAuthState } from "react-firebase-hooks/auth";
 import { IProfile } from ".";
+import { auth } from "../firebase/firebase";
+import { use, useEffect, useState } from "react";
+import { Header } from "../ui/header";
+import { Navbar } from "../ui/sidebar";
+import { getCurrentUser } from "../firebase/db";
 
-export default async function Profile({params}:IProfile){
-    const { username } = await params;
+export default function Profile({params}:IProfile){
+    const [user, loading] = useAuthState(auth);
+    const { username } = use(params);
+
+      const [name, setName] = useState<string>();
+      
+      useEffect(()=>{
+        if(!user) return;
+        getCurrentUser(user!.uid).then(data=>{
+          setName(data!.Name);
+        });
+      },[user]);
+
+    if (loading) return <p>Loading...</p>;
     return(
         <>
+            <Header/>
+            <Navbar name={name??''} username={username??''} />
             
         </>
     )
